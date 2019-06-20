c98ef2d2e614eaefc6734d60e605b2b1d2ead6c8
juger <juger@jugers-Mac.local>
2019-06-20 02:13:31 -0700

    Adding Menu + some settings

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by juger on 6/20/19.
+//  Copyright © 2019 juger. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.width - 60 
+    }
+
+}
