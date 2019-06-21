d48254b0912f765469f9243518a42f2ae88ce1de
juger <juger@jugers-Mac.local>
2019-06-21 01:51:11 -0700

    Finishing Login VC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by juger on 6/21/19.
+//  Copyright © 2019 juger. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //Actions -:
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    }
+    @IBAction func signupBtnPressed(_ sender: Any) {
+    }
+    
+    
+
+}
---
+++ Smack/Utilities/Contants.swift
@@ -0,0 +1,15 @@
+//
+//  Contants.swift
+//  Smack
+//
+//  Created by juger on 6/21/19.
+//  Copyright © 2019 juger. All rights reserved.
+//
+
+import Foundation
+
+
+
+//Segues
+let TO_LOGIN = "toLogin"
+
